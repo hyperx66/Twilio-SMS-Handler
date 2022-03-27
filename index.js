c0523b18e8cad4897f7a6bfcc3065cd60eb06798
Muhammad Haiqal <hyperx66@gmail.com>
2022-03-27 18:29:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express()
 var customerName, customerNumber, className;
 
 app.get('/', function(req, res) {
-    const accountSid = 'AC868944d86bfe426798bdc2864a9dd312';
-    const authToken = '849ad6b5da8224b0a3ec9b71e46ddf01';
+    const accountSid = '<insert sid here>';
+    const authToken = '<insert auth token here>';
     const client = require('twilio')(accountSid, authToken);
 
     var msgText = req.query("customerName") + " has expressed her interest to sign up for " + req.query("className") + ". Please contact the customer at " + req.query("customerNumber") + " for further clarification";
@@ -12,8 +12,8 @@ app.get('/', function(req, res) {
         .create({
             body: msgText,
             from: 'FSHNMKSPCE',
-            messagingServiceSid: 'MGdf8d0797cd98efd3788e42be11b766da',
-            to: '+6582920921'
+            messagingServiceSid: '',
+            to: '<recipient number with country code>'
         })
         .then(message => console.log(message.sid))
         .done();
